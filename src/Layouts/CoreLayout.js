30e6037b440cddfb7d58b64e4e5990ae722217d1
Andrew <sebastiandex@gmail.com>
2021-04-19 22:24:27 +1000

    начинаем добавлять минимальные стили

--- src/Layouts/CoreLayout.js
+++ src/Layouts/CoreLayout.js
@@ -1,13 +1,14 @@
 import React from 'react-dom';
 import Calendar from "../Components/Calendar";
 import ItemsLayout from "./ItemsLayout";
+import style from './coreLayout.module.scss'
 
 const CoreLayout = () => {
     return (
-        <>
+        <div className={style.main}>
         <Calendar />
         <ItemsLayout />
-        </>
+        </div>
     )
 }
 
